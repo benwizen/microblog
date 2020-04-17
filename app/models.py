d997e8f490f504da02ca59c7e31e20ea7088b01d
Ben Wizen <bwizen@infinidat.com>
2020-04-17 15:17:10 +0300

    Chapter 6

--- app/models.py
+++ app/models.py
@@ -1,7 +1,10 @@
 from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
-from app import db, login
+from hashlib import md5
+
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db, login
 
 
 class User(UserMixin, db.Model):
@@ -10,6 +13,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -20,6 +25,10 @@ class User(UserMixin, db.Model):
     @login.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
