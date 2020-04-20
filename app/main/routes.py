6e08f8b90a39dff6ff33e7f79342e30dfcfe5a3b
Ben Wizen <bwizen@infinidat.com>
2020-04-20 17:13:25 +0300

    Chapter 21

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,10 +6,10 @@ from flask_babel import _, get_locale
 from guess_language import guess_language
 from app import db
 from app.main.forms import EditProfileForm, PostForm
-from app.models import User, Post
+from app.models import User, Post, Message, Notification
 from app.translate import translate
 from app.main import bp
-from app.main.forms import SearchForm
+from app.main.forms import SearchForm, MessageForm
 
 
 @bp.before_app_request
@@ -158,8 +158,52 @@ def search():
 @bp.route('/remove_post/<post_id>', methods=['POST'])
 @login_required
 def remove_post(post_id):
-    flash(_(f'Removed post {post_id}! '))
+    flash(f'Removed post {post_id}! ')
     Post.query.filter_by(id=post_id).delete()
     db.session.commit()
     redirect_to = request.referrer or url_for('main.index')
     return redirect(redirect_to)
+
+
+@bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    user = User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(author=current_user, recipient=user, body=form.message.data)
+        db.session.add(msg)
+        user.add_notification('unread_message_count', user.new_messages())
+        db.session.commit()
+        flash(_('Your message has been sent.'))
+        return redirect(url_for('main.user', username=recipient))
+    return render_template('send_message.html', title=_('Send Message'), form=form, recipient=recipient)
+
+
+@bp.route('/messages')
+@login_required
+def messages():
+    current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
+    db.session.commit()
+
+    page = request.args.get('page', 1, type=int)
+    messages = current_user.messages_received.order_by(Message.timestamp.desc()).paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('main.messages', page=messages.next_num) if messages.has_next else None
+    prev_url = url_for('main.messages', page=messages.prev_num) if messages.has_prev else None
+    return render_template('messages.html', messages=messages.items, next_url=next_url, prev_url=prev_url)
+
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    notifications = current_user.notifications.filter(
+        Notification.timestamp > since).order_by(Notification.timestamp.asc())
+    return jsonify([{
+        'name': notification.name,
+        'data': notification.get_data(),
+        'timestamp': notification.timestamp
+    } for notification in notifications])
